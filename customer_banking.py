bedf052e0f56986d6f0a0144d73ef0a74c4fecd3
skiluvia <skiluvia@hotmail.com>
2023-11-12 01:49:22 -0500

    adding completed functioning application more stuff

--- customer_banking.py
+++ customer_banking.py
@@ -22,11 +22,13 @@ def main():
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-    print(f"Interest earned: {interest_earned:.2f}; and your savings balance is: {updated_savings_balance:.2f}")
+    print("----------------------------------------")
+    print(f"Interest earned for {savings_maturity} months is: {interest_earned:.2f} and your savings balance is: {updated_savings_balance:.2f}")
+    print("----------------------------------------")
     
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
-    cd_balance = float(input("Set your CD balance: "))
+    cd_balance = float(input("\nSet your CD balance: "))
     cd_interest = float(input("Set your CD interest rate: "))
     cd_maturity = int(input("Set length of months for your CD: "))
 
@@ -35,8 +37,10 @@ def main():
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-    print(f"Interest earned: {interest_earned:.2f} and your CD balance is: {updated_cd_balance:.2f}")
-
+    print("----------------------------------------")
+    print(f"Interest earned for {cd_maturity} months is: {interest_earned:.2f} and your CD balance is: {updated_cd_balance:.2f}")
+    print("----------------------------------------")
+    
 if __name__ == "__main__":
     # Call the main function.
     main()
